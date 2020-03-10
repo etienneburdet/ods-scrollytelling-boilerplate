5558bb6b6fef373508e59a3fb670d3c7977cd009
Etienne Burdet <etienne.burdet@gmail.com>
2020-03-10 10:28:02 +0100

    stable with imports

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import $ from 'jquery'
+// import angular from 'angular'
+// angular.module('ods-widgets', [require('angular-sanitize')])
+
+import './style.scss'
+
+$( "button.continue" ).html( "Next Step..." )
+// import angular from 'angular'
+//
+// angular.module('ods-widgets', [
+//   require('angular-sanitize')
+// ])
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/app.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: 'app.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  externals: {
+    jquery: 'jQuery',
+    angular: 'angular',
+    ngSanitize: 'angular-sanitize',
+    "ods-widgets": 'ods-widgets'
+  },
+  module: {
+    rules: [{
+      test: /\.s[ac]ss$/i,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ],
+    }, ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
 };
