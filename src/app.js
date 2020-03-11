22a6cb72c5e376ddd76cf5ada8917f65587ac94a
Etienne Burdet <etienne.burdet@gmail.com>
2020-03-11 16:27:12 +0100

    rebundled for webpack

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-import './assets/stylesheets/ods/ods-widgets.less'
+import './plugins/ods-widgets/stylesheets/ods-widgets.less'
 import './assets/stylesheets/app.scss'
+
+import mapConfig from './plugins/mapbox-storytelling/storytelling.js'
+
+import { storyConfig } from './components/story/config.js'
+import './components/story/story.scss'
+
+mapConfig(storyConfig)
