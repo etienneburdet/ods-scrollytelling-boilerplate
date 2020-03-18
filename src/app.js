0731e618464930f940748a56fca65e1ac16dab90
Etienne Burdet <etienne.burdet@gmail.com>
2020-03-18 11:40:39 +0100

    set up contex

--- src/app.js
+++ src/app.js
@@ -1,10 +1,2 @@
 import './plugins/ods-widgets/stylesheets/ods-widgets.less'
-import 'mapbox-gl/dist/mapbox-gl.css'
 import './assets/stylesheets/app.scss'
-
-import mapConfig from './plugins/mapbox-storytelling/storytelling.js'
-
-import { storyConfig } from './components/story/config.js'
-import './components/story/story.scss'
-
-mapConfig(storyConfig)
