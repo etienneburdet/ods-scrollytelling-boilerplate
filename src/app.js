686644625f08ded3564059d3d69e465a5034bbb7
Etienne Burdet <etienne.burdet@gmail.com>
2020-04-02 11:32:14 +0200

    add service worker

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
 import './plugins/ods-widgets/stylesheets/ods-widgets.less'
 import './assets/stylesheets/app.scss'
-import 'bootstrap';
+import 'bootstrap'
+
+const registerSW = () => {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(registration => {
+      console.log('SW registered: ', registration)
+    })
+    .catch(registrationError => {
+      console.log('SW registration failed: ', registrationError)
+    })
+}
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', registerSW)
+}
