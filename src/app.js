69993552cfb169b3239497ca96cee7cd937eae1f
Etienne Burdet <etienne.burdet@gmail.com>
2020-04-06 14:31:02 +0200

    Clean old app and simplify structure

--- src/app.js
+++ src/app.js
@@ -1,17 +1,3 @@
 import './plugins/ods-widgets/stylesheets/ods-widgets.less'
 import './assets/stylesheets/app.scss'
 import 'bootstrap'
-
-const registerSW = () => {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(registration => {
-      console.log('SW registered: ', registration)
-    })
-    .catch(registrationError => {
-      console.log('SW registration failed: ', registrationError)
-    })
-}
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', registerSW)
-}
